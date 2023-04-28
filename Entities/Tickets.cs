fdccf94b372cb69431a6022f93f0be39b4077e73
Madhan KAMALAKANNAN <112534933+MadhanKAMALAKANNAN@users.noreply.github.com>
2023-04-28 17:20:12 +1200

    Update Tickets.cs

--- Entities/Tickets.cs
+++ Entities/Tickets.cs
@@ -1,4 +1,10 @@
-﻿using System;
+/*
+ Created by : MADHAN KAMALAKANNAN
+ Student ID : 20230907
+ Email      : 20230907@mywhitecliffe.com
+ Date       : 31 March 2023
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
